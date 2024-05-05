297617257e3df2f01cc6da5b2b1b0de560095bc6
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-05 21:19:54 +0300

    Search places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  const base_url = 'http://0.0.0.0:5001/api/v1/';
+
+  $.ajax({
+      url: base_url + 'places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function(data) {
+          data.forEach(function(place) {
+              const article = $('<article></article>');
+              article.append(
+              '<div class="title_box"><h2>'
+              + place.name +
+              '</h2><div class="price_by_night">'
+              + place.price_by_night +
+              '</div></div>'
+            );
+              article.append(
+                '<div class="information">' +
+                  '<div class="max_guest">' +
+                  place.max_guest +
+                  ' Guest' +
+                  (place.max_guest !== 1 ? 's' : '') +
+                  '</div>' +
+                  '<div class="number_rooms">' + place.number_rooms +
+                  ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') +
+                  '</div>' +
+                  '<div class="number_bathrooms">' + place.number_bathrooms +
+                  ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') +
+                  '</div>' +
+                '</div>');
+              article.append('<div class="description">' + place.description + '</div>');
+              $('section.places').append(article);
+          });
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.log("Error:", textStatus, errorThrown);
+      }
+  });
+  $.get(base_url + 'status/', function(data){
+    const status = data.status;
+    if (status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+        $('div#api_status').removeClass('available');
+    }
+  });
+
+  const checkedAmenities = {};
+  $('div.amenities .popover ul li input[type="checkbox"]').change(function() {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+      if ($(this).is(':checked')) {
+          checkedAmenities[amenityId] = amenityName;
+      } else {
+          delete checkedAmenities[amenityId];
+      }
+      const amenitiesList = Object.values(checkedAmenities).join(', ');
+      $('.amenities h4').text('Checked amenities: ' + amenitiesList);
+  });
+});
